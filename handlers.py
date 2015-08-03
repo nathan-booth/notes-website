795677487a532e408ae723f1389c2331e5cce434
Nathan <boothnj@gmail.com>
2015-08-03 10:19:25 -0500

    Initial
    Templates and content

---
+++ handlers.py
@@ -0,0 +1,27 @@
+import os
+import webapp2
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+							   autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class MainPaige(Handler):
+	def get(self):
+		items = self.request.get_all("food")
+		self.render("course_page.html", items = items)
+		
+app = webapp2.WSGIApplication([('/', MainPaige),
+								],
+								debug=True)
