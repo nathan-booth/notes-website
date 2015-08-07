460ac6e3daf956649b6f59624f5e5d38f0f82239
Nathaniel Booth <boothnj@gmail.com>
2015-08-06 20:29:30 -0500

    Solved blank validation issue

--- handlers.py
+++ handlers.py
@@ -5,82 +5,78 @@ from google.appengine.ext import ndb
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-							   autoescape=True)
+                               autoescape=True)
 
 class Handler(webapp2.RequestHandler):
-	def write(self, *a, **kw):
-		self.response.out.write(*a, **kw)
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
-	def render_str(self, template, **params):
-		t = jinja_env.get_template(template)
-		return t.render(params)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
-	def render(self, template, **kw):
-		self.write(self.render_str(template, **kw))
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
 class CoursePaige(Handler):
-	def get(self):
-		self.render("course_toc.html")
+    def get(self):
+        self.render("course_toc.html")
 
 class Stage1(Handler):
-	def get(self):
-		self.render("stage1.html")
+    def get(self):
+        self.render("stage1.html")
 
 class Stage2(Handler):
-	def get(self):
-		self.render("stage2.html")
+    def get(self):
+        self.render("stage2.html")
 
 class Stage3(Handler):
-	def get(self):
-		self.render("stage3.html")
+    def get(self):
+        self.render("stage3.html")
 
 class Stage4(Handler):
-	def get(self):
-		self.render("stage4.html")
+    def get(self):
+        self.render("stage4.html")
 
 class Stage5(Handler):
-	def get(self):
-		self.render("stage5.html")
-
-class Error(Handler):
-	def get(self):
-		self.render("error.html")
+    def get(self):
+        self.render("stage5.html")
 
 class Comment(ndb.Model):
-	name = ndb.StringProperty()
-	comment_content = ndb.StringProperty()
-	date = ndb.DateTimeProperty(auto_now_add=True)
+    name = ndb.StringProperty()
+    comment_content = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
 class CommentsHandler(CoursePaige):
-	def get(self):
-		comments_query = Comment.query(ancestor=comment_key).order(-Comment.date)
-		comments = comments_query.fetch()
+    def get(self):
+        comments_query = Comment.query(ancestor=comment_key).order(-Comment.date)
+        comments = comments_query.fetch()
 
-		self.render("comments.html", comments=comments)
+        blank_error = self.request.get('blank_error')
 
-	def post(self):
-		comment = Comment(parent=comment_key)
-		comment.name = self.request.get('name')
-		comment.comment_content = self.request.get('comment_content')
+        self.render("comments.html", comments=comments, blank_error=blank_error)
 
-		# validation
-		if comment.name == '' or comment.comment_content == '':
-			blank_error = "Please, fill out both the name and comment sections."
-			self.redirect('/comments')
-		else:
-			comment.put()
+    def post(self):
+        comment = Comment(parent=comment_key)
+        comment.name = self.request.get('name')
+        comment.comment_content = self.request.get('comment_content')
+        blank_error = ''
 
-		self.redirect('/comments')
+        if comment.name == '' or comment.comment_content == '':
+            blank_error = "Please, fill out both the name and comment sections."
+        else:
+            comment.put()
+
+        self.redirect('/comments?blank_error='+blank_error)
 
 comment_key = ndb.Key('Comment', 'course_toc')
 
 app = webapp2.WSGIApplication([('/', CoursePaige),
-							   ('/stage1', Stage1),
-							   ('/stage2', Stage2),
-							   ('/stage3', Stage3),
-							   ('/stage4', Stage4),
-							   ('/stage5', Stage4),
-							   ('/comments', CommentsHandler),
-							   ('/error', Error)
-							   ],
-								debug=True)
+                               ('/stage1', Stage1),
+                               ('/stage2', Stage2),
+                               ('/stage3', Stage3),
+                               ('/stage4', Stage4),
+                               ('/stage5', Stage4),
+                               ('/comments', CommentsHandler)
+                               ],
+                                debug=True)
